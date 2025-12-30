32b44264275c7546cf961e2e4347dda0b759dd09
maziyank <maziyank@gmail.com>
2025-12-30 07:11:39 +0700

    fix prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema/base.prisma",
+  schema: "prisma/schema",
   migrations: {
     seed: "tsx prisma/seed.ts",
   },
